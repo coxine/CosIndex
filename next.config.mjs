7794d8211443b99bb318fd9c10943edc87fd0752
coxine <59347549+coxine@users.noreply.github.com>
2024-05-27 17:25:25 +0800

    +redirect for goCos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/nav',
+                permanent: false,
+            },
+            {   // don't redirect when path is nav or contains _ or .
+                source: '/:path((?!.*[._]|nav).*)',
+                destination: 'https://go.cos.tg/:path',
+                permanent: false,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
